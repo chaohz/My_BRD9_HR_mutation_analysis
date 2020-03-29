5951799874571da87ae73d7da5be4c8d78a55900
zc0329 <zhangchao0103@163.com>
2020-03-30 03:37:42 +0800

    提交信息

---
+++ BRD9_BRCA_high_sig3.R
@@ -0,0 +1,180 @@
+﻿ /opt/R/3.5.1/bin/R
+load("/home/chao/A3B/MAF/data/UVM/mutation/96_trinucleodide_mutation.Rdata")
+
+library(BSgenome)
+ref_genome <- "BSgenome.Hsapiens.UCSC.hg19"
+library(ref_genome, character.only = TRUE)
+library(MutationalPatterns)
+
+
+
+sp_url <- paste("https://cancer.sanger.ac.uk/cancergenome/assets/",
+ "signatures_probabilities.txt", sep = "")
+ 
+cancer_signatures = read.table(sp_url, sep = "\t", header = TRUE)
+new_order = match(row.names(mut_mat), cancer_signatures$Somatic.Mutation.Type)
+cancer_signatures = cancer_signatures[as.vector(new_order),]
+row.names(cancer_signatures) = cancer_signatures$Somatic.Mutation.Type
+cancer_signatures = as.matrix(cancer_signatures[,4:33])
+
+fit_res <- fit_to_signatures(mut_mat, cancer_signatures)
+#fit_res$contribution
+#fit_res$reconstructed
+
+#================================================================================================
+#================================================================================================
+HR_sig_num = fit_res$contribution["Signature.3", ]
+#A3B_sig_num = fit_res$contribution["Signature.13", ]
+
+setwd("/data/BRCA/BRCA_SNV_MUTECT2")
+#===================================================================
+#===================================================================
+mutData1 = read.csv(dir()[5], header=F, sep="\t", stringsAsFactors=F)
+mutData2=mutData1[-(1:5),1]
+rm(mutData1)
+mutData3=array(mutData2,dim=c(120,length(mutData2)/120))
+rm(mutData2)
+mutData=as.data.frame(t(mutData3),stringsAsFactors=F)
+rm(mutData3)
+mutM = mutData[-1,]
+names(mutM) = as.vector(as.matrix(mutData[1,]))
+rm(mutData)
+
+MCWmotif = mutM
+
+patients_with_BRD9_M_OV = substr(mutM[which(mutM[ ,"SYMBOL"] == "BRD9"), "Tumor_Sample_Barcode"] ,1 ,15)
+patients_with_BRD9_M_OV_index = patients_with_BRD9_M_OV[!duplicated(patients_with_BRD9_M_OV)]
+patients_with_BRCA_M_OV = substr(mutM[which((mutM[ ,"SYMBOL"] == "BRCA1") | (mutM[ ,"SYMBOL"] == "BRCA2")), "Tumor_Sample_Barcode"] ,1 ,15)
+patients_with_BRCA_M_OV_index = patients_with_BRCA_M_OV[!duplicated(patients_with_BRCA_M_OV)]
+
+patients_with_M_OV = substr(mutM[, "Tumor_Sample_Barcode"] ,1 ,15)
+patients_with_M_OV_index = patients_with_M_OV[!duplicated(patients_with_M_OV)]
+
+
+
+#setwd("E:/TCGA/data - 副本/OV/mutation")
+setwd("/home/chao/A3B/MAF/data/BRCA/BRCA_SNV_MUTECT2")
+#setwd("/home/chao/A3B/MAF/data/OV/mutation")
+#===================================================================
+#===================================================================
+mutData1 = read.csv(dir()[5], header=F, sep="\t", stringsAsFactors=F)
+mutData2=mutData1[-(1:5),1]
+rm(mutData1)
+mutData3=array(mutData2,dim=c(120,length(mutData2)/120))
+rm(mutData2)
+mutData=as.data.frame(t(mutData3),stringsAsFactors=F)
+rm(mutData3)
+mutM = mutData[-1,]
+names(mutM) = as.vector(as.matrix(mutData[1,]))
+rm(mutData)
+
+MCWmotif = mutM
+
+patients_with_BRD9_M_BR = substr(mutM[which(mutM[ ,"SYMBOL"] == "BRD9"), "Tumor_Sample_Barcode"] ,1 ,15)
+patients_with_BRD9_M_BR_index = patients_with_BRD9_M_BR[!duplicated(patients_with_BRD9_M_BR)]
+patients_with_BRCA_M_BR = substr(mutM[which((mutM[ ,"SYMBOL"] == "BRCA1") | (mutM[ ,"SYMBOL"] == "BRCA2")), "Tumor_Sample_Barcode"] ,1 ,15)
+patients_with_BRCA_M_BR_index = patients_with_BRCA_M_BR[!duplicated(patients_with_BRCA_M_BR)]
+
+patients_with_M_BR = substr(mutM[, "Tumor_Sample_Barcode"] ,1 ,15)
+patients_with_M_BR_index = patients_with_M_BR[!duplicated(patients_with_M_BR)]
+
+
+#setwd("E:/TCGA/data - 副本/OV/mutation")
+#setwd("/home/chao/A3B/MAF/data/BRCA/BRCA_SNV_MUTECT2")
+setwd("/home/chao/A3B/MAF/data/PAAD/mutation")
+#===================================================================
+#===================================================================
+mutData1 = read.csv(dir()[5], header=F, sep="\t", stringsAsFactors=F)
+mutData2=mutData1[-(1:5),1]
+rm(mutData1)
+mutData3=array(mutData2,dim=c(120,length(mutData2)/120))
+rm(mutData2)
+mutData=as.data.frame(t(mutData3),stringsAsFactors=F)
+rm(mutData3)
+mutM = mutData[-1,]
+names(mutM) = as.vector(as.matrix(mutData[1,]))
+rm(mutData)
+
+MCWmotif = mutM
+
+patients_with_BRD9_M_PA = substr(mutM[which(mutM[ ,"SYMBOL"] == "BRD9"), "Tumor_Sample_Barcode"] ,1 ,15)
+patients_with_BRD9_M_PA_index = patients_with_BRD9_M_PA[!duplicated(patients_with_BRD9_M_PA)]
+patients_with_BRCA_M_PA = substr(mutM[which((mutM[ ,"SYMBOL"] == "BRCA1") | (mutM[ ,"SYMBOL"] == "BRCA2")), "Tumor_Sample_Barcode"] ,1 ,15)
+patients_with_BRCA_M_PA_index = patients_with_BRCA_M_PA[!duplicated(patients_with_BRCA_M_PA)]
+
+patients_with_M_PA = substr(mutM[, "Tumor_Sample_Barcode"] ,1 ,15)
+patients_with_M_PA_index = patients_with_M_PA[!duplicated(patients_with_M_PA)]
+
+#setwd("E:/TCGA/data - 副本/OV/mutation")
+#setwd("/home/chao/A3B/MAF/data/BRCA/BRCA_SNV_MUTECT2")
+setwd("/home/chao/A3B/MAF/data/PRAD/mutation")
+#===================================================================
+#===================================================================
+mutData1 = read.csv(dir()[5], header=F, sep="\t", stringsAsFactors=F)
+mutData2=mutData1[-(1:5),1]
+rm(mutData1)
+mutData3=array(mutData2,dim=c(120,length(mutData2)/120))
+rm(mutData2)
+mutData=as.data.frame(t(mutData3),stringsAsFactors=F)
+rm(mutData3)
+mutM = mutData[-1,]
+names(mutM) = as.vector(as.matrix(mutData[1,]))
+rm(mutData)
+
+MCWmotif = mutM
+
+
+patients_with_BRD9_M_PR = substr(mutM[which(mutM[ ,"SYMBOL"] == "BRD9"), "Tumor_Sample_Barcode"] ,1 ,15)
+patients_with_BRD9_M_PR_index = patients_with_BRD9_M_PR[!duplicated(patients_with_BRD9_M_PR)]
+patients_with_BRCA_M_PR = substr(mutM[which((mutM[ ,"SYMBOL"] == "BRCA1") | (mutM[ ,"SYMBOL"] == "BRCA2")), "Tumor_Sample_Barcode"] ,1 ,15)
+patients_with_BRCA_M_PR_index = patients_with_BRCA_M_PR[!duplicated(patients_with_BRCA_M_PR)]
+
+patients_with_M_PR = substr(mutM[, "Tumor_Sample_Barcode"] ,1 ,15)
+patients_with_M_PR_index = patients_with_M_PR[!duplicated(patients_with_M_PR)]
+
+#===================================================================
+#combine four cancer types
+#===================================================================
+patients_with_BRD9_M_index = union(union(patients_with_BRD9_M_OV_index, patients_with_BRD9_M_BR_index), union(patients_with_BRD9_M_PA_index, patients_with_BRD9_M_PR_index))
+patients_with_BRCA_M_index = union(union(patients_with_BRCA_M_OV_index, patients_with_BRCA_M_BR_index), union(patients_with_BRCA_M_PA_index, patients_with_BRCA_M_PR_index))
+patients_with_M_index = union(union(patients_with_M_OV_index, patients_with_M_BR_index), union(patients_with_M_PA_index, patients_with_M_PR_index))
+
+#patients_with_BRD9_M_index = patients_with_BRD9_M_OV_index
+#patients_with_BRCA_M_index = patients_with_BRCA_M_OV_index
+#patients_with_M_index = patients_with_M_OV_index
+
+HR_sig_num = fit_res$contribution["Signature.3", ]
+#A3B_sig_num = fit_res$contribution["Signature.13", ]
+
+patients_with_BRD9_M_HRsig = intersect(patients_with_BRD9_M_index, names(HR_sig_num))
+patients_with_BRCA_M_HRsig = intersect(patients_with_BRCA_M_index, names(HR_sig_num))
+
+patients_with_M_HRsig = intersect(patients_with_M_index, names(HR_sig_num))
+HR_sig_num_with_M_HRsig = HR_sig_num[patients_with_M_HRsig]
+
+patients_with_M_HRsig_high = names(HR_sig_num_with_M_HRsig)[which(HR_sig_num_with_M_HRsig > fivenum(HR_sig_num_with_M_HRsig)[4])]
+
+
+patients_with_M_HRsig_high_BRD9_BRCA = intersect(patients_with_M_HRsig_high, intersect(patients_with_BRD9_M_HRsig, patients_with_BRCA_M_HRsig))
+patients_with_M_HRsig_high_BRD9 = intersect(patients_with_M_HRsig_high, patients_with_BRD9_M_HRsig)[- which(intersect(patients_with_M_HRsig_high, patients_with_BRD9_M_HRsig) %in% patients_with_M_HRsig_high_BRD9_BRCA)]
+patients_with_M_HRsig_high_BRCA = intersect(patients_with_M_HRsig_high, patients_with_BRCA_M_HRsig)[- which(intersect(patients_with_M_HRsig_high, patients_with_BRCA_M_HRsig) %in% patients_with_M_HRsig_high_BRD9_BRCA)]
+patients_with_M_HRsig_high_null = patients_with_M_HRsig_high[- which(patients_with_M_HRsig_high %in% union(union(patients_with_M_HRsig_high_BRD9_BRCA, patients_with_M_HRsig_high_BRD9), patients_with_M_HRsig_high_BRCA))]
+
+
+
+BRD9_mutation_array = data.frame(rep(0,length(patients_with_M_HRsig_high)))
+rownames(BRD9_mutation_array ) = patients_with_M_HRsig_high
+names(BRD9_mutation_array ) = "BRD9"
+BRD9_mutation_array[union(patients_with_M_HRsig_high_BRD9, patients_with_M_HRsig_high_BRD9_BRCA),] = 1
+
+BRCA_mutation_array = data.frame(rep(0,length(patients_with_M_HRsig_high)))
+rownames(BRCA_mutation_array) = patients_with_M_HRsig_high
+names(BRCA_mutation_array) = "BRCA"
+BRCA_mutation_array[union(patients_with_M_HRsig_high_BRCA, patients_with_M_HRsig_high_BRD9_BRCA),] = 1
+
+mydata = data.frame(BRD9_mutation_array, BRCA_mutation_array)
+mydata = table(mydata)
+
+#chisq.test(mydata)
+fisher.test(mydata,alternative ="less")
+
---
+++ Sig_3_Gene_Mutation_Cor.r
@@ -0,0 +1,79 @@
+﻿ /opt/R/3.5.1/bin/R
+load("/home/chao/A3B/MAF/data/UVM/mutation/96_trinucleodide_mutation.Rdata")
+
+library(BSgenome)
+ref_genome <- "BSgenome.Hsapiens.UCSC.hg19"
+library(ref_genome, character.only = TRUE)
+library(MutationalPatterns)
+
+
+
+sp_url <- paste("https://cancer.sanger.ac.uk/cancergenome/assets/",
+ "signatures_probabilities.txt", sep = "")
+ 
+cancer_signatures = read.table(sp_url, sep = "\t", header = TRUE)
+new_order = match(row.names(mut_mat), cancer_signatures$Somatic.Mutation.Type)
+cancer_signatures = cancer_signatures[as.vector(new_order),]
+row.names(cancer_signatures) = cancer_signatures$Somatic.Mutation.Type
+cancer_signatures = as.matrix(cancer_signatures[,4:33])
+
+fit_res <- fit_to_signatures(mut_mat, cancer_signatures)
+#fit_res$contribution
+#fit_res$reconstructed
+
+
+#================================================================================================
+#================================================================================================
+HR_sig_num = fit_res$contribution["Signature.3", ]
+
+setwd("/home/chao/A3B/MAF/data/OV/mutation")
+#===================================================================
+#===================================================================
+mutData1 = read.csv(dir()[5], header=F, sep="\t", stringsAsFactors=F)
+mutData2=mutData1[-(1:5),1]
+rm(mutData1)
+mutData3=array(mutData2,dim=c(120,length(mutData2)/120))
+rm(mutData2)
+mutData=as.data.frame(t(mutData3),stringsAsFactors=F)
+rm(mutData3)
+mutM = mutData[-1,]
+names(mutM) = as.vector(as.matrix(mutData[1,]))
+rm(mutData)
+
+MCWmotif = mutM
+
+#patients_with_EGFR_M = substr(mutM[which((mutM[ ,"SYMBOL"] == "EGFR") & (mutM[ ,"Variant_Classification"] != "RNA") & (mutM[ ,"Variant_Classification"] != "Silent")), "Tumor_Sample_Barcode"] ,1 ,15)
+#patients_with_EGFR_M = substr(mutM[which((mutM[ ,"SYMBOL"] == "EGFR") & (mutM[ ,"Variant_Classification"] != "Silent")), "Tumor_Sample_Barcode"] ,1 ,15)
+patients_with_EGFR_M = substr(mutM[which(mutM[ ,"SYMBOL"] == "BRD9"), "Tumor_Sample_Barcode"] ,1 ,15)
+patients_with_EGFR_M = patients_with_EGFR_M[!duplicated(patients_with_EGFR_M)]
+
+patients_with_M = substr(mutM[, "Tumor_Sample_Barcode"] ,1 ,15)
+patients_with_M = patients_with_M[!duplicated(patients_with_M)]
+
+rm(mutM)
+
+commonpatients = intersect(names(HR_sig_num), patients_with_M)
+patients_EGFR_M = intersect(patients_with_EGFR_M, commonpatients)
+HR_sig_num = HR_sig_num[commonpatients]
+
+mutation_array = data.frame(rep(0,length(commonpatients)))
+rownames(mutation_array) = commonpatients 
+names(mutation_array) = "mutation"
+mutation_array[patients_EGFR_M,] = 1
+
+m_threshold = fivenum(HR_sig_num)[4]
+#m_threshold = mean(HR_sig_num)
+#m_threshold = sort(HR_sig_num)[round(length(HR_sig_num)*0.70)]
+
+express_array = data.frame(rep(0,length(commonpatients)))
+rownames(express_array) = commonpatients
+names(express_array) = "expression"
+express_array[which(HR_sig_num > m_threshold), ] = 1
+
+#=====================================================================================
+mydata = data.frame(mutation_array, express_array)
+#mydata = table(mutation_array[,1], express_array[,1])
+mydata = table(mydata)
+
+#chisq.test(mydata)
+fisher.test(mydata,alternative ="greater")
